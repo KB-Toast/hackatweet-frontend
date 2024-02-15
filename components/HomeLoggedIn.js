9157a1913d96a4a7575e913eca391390d903c378
KB-Toast <kevin.brusson@gmail.com>
2024-02-15 12:51:30 +0100

    switching component homeloggedin to page

--- components/HomeLoggedIn.js
+++
@@ -1,7 +0,0 @@
-import styles from '../styles/HomeLoggedIn.module.css';
-
-function HomeLoggedIn() {
-  return (<div>Home logged in</div>);
-}
-
-export default HomeLoggedIn;
---
+++ pages/homeLoggedIn.js
@@ -0,0 +1,7 @@
+import styles from '../styles/HomeLoggedIn.module.css';
+
+function Index() {
+  return <div>Page Home Logged In</div>;
+}
+
+export default Index;
